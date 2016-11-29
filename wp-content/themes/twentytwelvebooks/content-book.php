8fd3016c226d19e6457ef249848012c1f4c1a364
Dewey Dunnington <dewey@fishandwhistle.net>
2016-11-29 13:37:37 -0500

    add commas between genres and keywords; remove author bio

--- wp-content/themes/twentytwelvebooks/content-book.php
+++ wp-content/themes/twentytwelvebooks/content-book.php
@@ -46,31 +46,11 @@
 		</div><!-- .entry-summary -->
 		<footer class="entry-meta">
                     <div></div>
-                    <?php the_terms($post->ID, 'genres', '<span class="book-terms">Genres: ', $after='</span>') ?>
-                    <?php the_terms($post->ID, 'keywords', '<span class="book-terms">Keywords: ', $after='</span>') ?>
+                    <?php the_terms($post->ID, 'genres', '<span class="book-terms">Genres: ', $sep=', ', $after='</span>') ?>
+                    <?php the_terms($post->ID, 'keywords', '<span class="book-terms">Keywords: ', $sep=', ', $after='</span>') ?>
                     <div>
 			<?php twentytwelve_entry_meta(); ?>
 			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-			<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries. ?>
-				<div class="author-info">
-					<div class="author-avatar">
-						<?php
-						/** This filter is documented in author.php */
-						$author_bio_avatar_size = apply_filters( 'twentytwelve_author_bio_avatar_size', 68 );
-						echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
-						?>
-					</div><!-- .author-avatar -->
-					<div class="author-description">
-						<h2><?php printf( __( 'About %s', 'twentytwelve' ), get_the_author() ); ?></h2>
-						<p><?php the_author_meta( 'description' ); ?></p>
-						<div class="author-link">
-							<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-								<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentytwelve' ), get_the_author() ); ?>
-							</a>
-						</div><!-- .author-link	-->
-					</div><!-- .author-description -->
-				</div><!-- .author-info -->
-			<?php endif; ?>
                     </div>
 		</footer><!-- .entry-meta -->
 	</article><!-- #post -->
